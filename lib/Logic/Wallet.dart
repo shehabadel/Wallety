9ebc9cec8c63053e5be57c5705c8b6492b8bf565
shehabadel <shehabeldinadelx@gmail.com>
2020-09-13 18:24:43 +0200

    added the UI cardDetails design, started building the business logic, check TODO in Wallets Class

---
+++ lib/Logic/Wallet.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+import 'dart:io';
+
+class Wallet {
+  String walletTitle;
+  final double initialValue;
+  double _currentValue;
+  var transactionsList = new List();
+  Wallet(this.initialValue, this.walletTitle) {
+    var newFile = new File('$walletTitle.txt');
+    //TODO read farah's assignment and implement opening file and appending Transaction data
+    //TODO and read from a file the transaction data
+  }
+}
--- lib/UI/CardDetails.dart
+++ lib/UI/CardDetails.dart
@@ -3,8 +3,15 @@ import 'package:flutter/material.dart';
 
 class CardDetails extends StatelessWidget {
   String Test;
-  CardDetails(String Test) {
-    this.Test = Test;
+  String assetName = 'assets/Expense.svg';
+  String expenseDesc;
+  String Date;
+  String expenseValue;
+
+  CardDetails(String expenseDesc, String Date, String expenseValue) {
+    this.expenseDesc = expenseDesc;
+    this.Date = Date;
+    this.expenseValue = expenseValue;
   }
   @override
   Widget build(BuildContext context) {
@@ -12,14 +19,36 @@ class CardDetails extends StatelessWidget {
     return Padding(
         padding: const EdgeInsets.all(8.0),
         child: Material(
-          color: Colors.redAccent,
+          color: Colors.white,
           elevation: 14.0,
           borderRadius: BorderRadius.circular(24.0),
           shadowColor: Color(0x802196F3),
-          child: Text(
-            "$Test",
-            style: TextStyle(fontSize: 20.0),
-          ),
+          child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(5.0),
+                    child: Image.asset('assets/images/Expense.png'),
+                  ),
+                  Text(
+                    "$expenseDesc",
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "$expenseValue EGP",
+                        style: TextStyle(fontSize: 15.0),
+                      ),
+                      Text("$Date", style: TextStyle(fontSize: 15.0))
+                    ],
+                  )
+                ],
+              )),
         ));
   }
 }
